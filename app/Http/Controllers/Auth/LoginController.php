c76a08acd33bc807d5555478740f976e02b26252
ahmad rifai <riff.blend@gmail.com>
2020-05-10 19:28:52 +0700

    init commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected $fieldToCheck;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+
+        $login = request()->input('login');
+
+        //if user input is in email format, use email
+        if (filter_var($login, FILTER_VALIDATE_EMAIL)){
+            $this->fieldToCheck = "email";
+        } else {
+            $this->fieldToCheck = "username";
+        }
+
+        request()->merge([
+            $this->fieldToCheck => $login //add new field to existing request
+        ]);
+
+    }
+
+    public function username()
+    {
+        return $this->fieldToCheck;
+    }
+}
